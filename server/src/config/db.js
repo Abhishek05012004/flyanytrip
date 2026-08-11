39e5b2cc50375b7883de791e0600a7be2c9c0810
Abhishek Jha <abhishekjha2707@gmail.com>
2026-08-11 18:52:23 +0530

    seat selection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,18 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-// In production, reuse a single Prisma client instance
+const connectionString = process.env.DATABASE_URL;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
 const globalForPrisma = globalThis;
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: ["query", "error", "warn"],
   });
 
